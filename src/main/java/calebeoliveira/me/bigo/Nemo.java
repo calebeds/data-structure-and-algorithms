30111541a9cfd16253e6e7ff704c5ada64223de0
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-03-18 16:13:49 -0300

    change find nemo to show how many times it runs

--- src/main/java/calebeoliveira/me/bigo/Nemo.java
+++ src/main/java/calebeoliveira/me/bigo/Nemo.java
@@ -5,23 +5,25 @@ import java.util.Arrays;
 public class Nemo {
 
     public static void main(String[] args) {
-//        String[] nemo = {"nemo"};
-//        String[] everyone = {"dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"};
-//        String[] large = Arrays.stream(new String[100000]).map(l -> "nemo").toArray(String[]::new);
-//
-//        findNemo(large);  // O(n) --> Linear Time
+        String[] nemo = {"nemo"};
+        String[] everyone = {"dory", "bruce", "marlin", "gill", "bloat", "nigel", "squirt", "darla", "hank", "nemo"};
+        String[] large = Arrays.stream(new String[100000]).map(l -> "nemo").toArray(String[]::new);
 
-        int[] boxes = {0, 1, 2, 3, 4, 5};
-        printFirstTwoBoxes(boxes); // O(2) - Constante Time
+        findNemo(large);  // O(n) --> Linear Time
+
+//        int[] boxes = {0, 1, 2, 3, 4, 5};
+//        printFirstTwoBoxes(boxes); // O(2) - Constante Time
     }
 
-//    public static void findNemo(String[] array) {
-//        for (int i = 0; i < array.length; i++) {
-//            if ("nemo".equals(array[i])) {
-//                System.out.println("Found NEMO!");
-//            }
-//        }
-//    }
+    public static void findNemo(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("running");
+            if ("nemo".equals(array[i])) {
+                System.out.println("Found NEMO!");
+                break;
+            }
+        }
+    }
 
     public static void printFirstTwoBoxes(int[] boxes) {
         System.out.println(boxes[0]); // O(1)
