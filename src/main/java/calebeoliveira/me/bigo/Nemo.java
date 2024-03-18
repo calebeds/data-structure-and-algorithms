884b75cab5fa27ca08497465e17415b17a2da6fa
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-03-18 14:45:42 -0300

    add comment to indicate first big O notation: linear time - O(n)

--- src/main/java/calebeoliveira/me/bigo/Nemo.java
+++ src/main/java/calebeoliveira/me/bigo/Nemo.java
@@ -9,17 +9,14 @@ public class Nemo {
         String[] everyone = {"dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"};
         String[] large = Arrays.stream(new String[100000]).map(l -> "nemo").toArray(String[]::new);
 
-        findNemo(large);
+        findNemo(large);  // O(n) --> Linear Time
     }
 
     public static void findNemo(String[] array) {
-        var t0 = System.currentTimeMillis();
         for (int i = 0; i < array.length; i++) {
-            if("nemo".equals(array[i])) {
+            if ("nemo".equals(array[i])) {
                 System.out.println("Found NEMO!");
             }
         }
-        var t1 = System.currentTimeMillis();
-        System.out.println("Call to find Nemo took " + (t1 - t0) + " milliseconds");
     }
 }
