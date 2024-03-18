31bd46f023d9cfe2601829c825e09e03a4231524
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-03-18 14:56:49 -0300

    add O(n) - Constante Time example

--- src/main/java/calebeoliveira/me/bigo/Nemo.java
+++ src/main/java/calebeoliveira/me/bigo/Nemo.java
@@ -5,18 +5,26 @@ import java.util.Arrays;
 public class Nemo {
 
     public static void main(String[] args) {
-        String[] nemo = {"nemo"};
-        String[] everyone = {"dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"};
-        String[] large = Arrays.stream(new String[100000]).map(l -> "nemo").toArray(String[]::new);
+//        String[] nemo = {"nemo"};
+//        String[] everyone = {"dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"};
+//        String[] large = Arrays.stream(new String[100000]).map(l -> "nemo").toArray(String[]::new);
+//
+//        findNemo(large);  // O(n) --> Linear Time
 
-        findNemo(large);  // O(n) --> Linear Time
+        int[] boxes = {0, 1, 2, 3, 4, 5};
+        printFirstTwoBoxes(boxes); // O(2) - Constante Time
     }
 
-    public static void findNemo(String[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if ("nemo".equals(array[i])) {
-                System.out.println("Found NEMO!");
-            }
-        }
+//    public static void findNemo(String[] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            if ("nemo".equals(array[i])) {
+//                System.out.println("Found NEMO!");
+//            }
+//        }
+//    }
+
+    public static void printFirstTwoBoxes(int[] boxes) {
+        System.out.println(boxes[0]); // O(1)
+        System.out.println(boxes[1]); // O(1)
     }
 }
