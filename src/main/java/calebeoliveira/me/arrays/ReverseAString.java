436ade3cbe1e95bf64eb84d600c0ea554ba5eec9
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-03-20 16:47:30 -0300

    add alternatives reverse array examples

--- src/main/java/calebeoliveira/me/arrays/ReverseAString.java
+++ src/main/java/calebeoliveira/me/arrays/ReverseAString.java
@@ -6,6 +6,10 @@ package calebeoliveira.me.arrays;
 public class ReverseAString {
 
     public static String reserveAString(String string) {
+        if(string == null || string.length() < 2)  {
+            return "No string provided";
+        }
+
         char[] arrString = new char[string.length()];
 
         for (int i = 0; i < string.length(); i++) {
@@ -20,7 +24,29 @@ public class ReverseAString {
         return new String(arrString);
     }
 
+    public static String reverseAString2(String string) {
+        String result = "";// It's more recommended to use "StringBuilder" object when manipulating strings
+        final int length = string.length();
+        for (int i = length - 1; i>=0; i--) {
+            result += string.charAt(i);
+        }
+        return result;
+    }
+
+    public static String reverseStringWithReelJava(String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
+
+    public static String recursiveReversal(String str) {
+        if(str.length() == 0) //breakpoint for recursion
+            return "";
+        return recursiveReversal(str.substring(1)) + str.charAt(0);
+    }
+
     public static void main(String[] args) {
         System.out.println(reserveAString("My name is Calebe"));
+        System.out.println(reverseAString2("My name is Calebe"));
+        System.out.println(reverseStringWithReelJava("My name is Calebe"));
+        System.out.println(recursiveReversal("My name is Calebe"));
     }
 }
