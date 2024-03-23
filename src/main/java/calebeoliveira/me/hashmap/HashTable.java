b37b831d52dc1b9cbb3350ab32dfc7e90258c4fe
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-03-23 16:55:33 -0300

    add hashtable example

---
+++ src/main/java/calebeoliveira/me/hashmap/HashTable.java
@@ -0,0 +1,40 @@
+package calebeoliveira.me.hashmap;
+
+public class HashTable {
+    Object[][] data;
+    public HashTable(int size) {
+        this.data = new Object[size][2];
+    }
+
+    private int hash(String key) {
+        int hash = 0;
+        for (int i = 0; i < key.length(); i++) {
+            hash = (hash + key.charAt(i) * i) % this.data.length;
+        }
+
+        return hash;
+    }
+
+    public void set(String key, Object value) {
+        int address = hash(key);
+
+        if(data[address] != null) {
+            data[address][0] = key;
+            data[address][1] = value;
+        }
+
+    }
+
+    public Object get(String key) {
+        int address = hash(key);
+        return data[address][1];
+    }
+
+    public static void main(String[] args) {
+        HashTable hashTable = new HashTable(5);
+        hashTable.set("grapes", 10000);
+        System.out.println(hashTable.get("grapes"));
+        hashTable.set("calebe", "dope");
+        System.out.println(hashTable.get("calebe"));
+    }
+}
---
+++ src/main/java/calebeoliveira/me/hashmap/HashTable2.java
@@ -0,0 +1,67 @@
+package calebeoliveira.me.hashmap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTable2 {
+    private static class MyNodes extends ArrayList<MyNode> {}
+    private final int length;
+    private MyNodes[] data;
+
+    public HashTable2(int size) {
+        this.length = size;
+        data = new MyNodes[length];
+    }
+
+    public void set(String key, int value) {
+        if(data[hash(key)] == null) {
+            data[hash(key)] = new MyNodes();
+        }
+        data[hash(key)].add(new MyNode(key, value));
+    }
+
+    public int get(String key) {
+        if(data[hash(key)] == null) {
+            return 0;
+        }
+        for (MyNode node: data[hash(key)]) {
+            if(node.getKey().equals(key)) {
+                return node.getValue();
+            }
+        }
+        return 0;
+    }
+
+    public List<String> keys() {
+        List<String> keys = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            if(data[i] != null) {
+                for (int j = 0; j < data[i].size(); j++) {
+                    keys.add(data[i].get(j).getKey());
+                }
+            }
+        }
+        return keys;
+    }
+
+    private int hash(String key) {
+        int hash = 0;
+
+        for(int i = 0; i < key.length(); i++) {
+            hash = (hash + key.charAt(i) * i) % length;
+        }
+
+        return hash;
+    }
+
+    public static void main(String[] args) {
+        HashTable2 table = new HashTable2(50);
+        table.set("ra", 1000);
+        table.set("foo", 1000);
+        table.set("bar", 1000);
+        System.out.println(table.keys().size());
+        System.out.println(table.get("ra"));
+        System.out.println(table.get("foo"));
+        System.out.println(table.get("bar"));
+    }
+}
